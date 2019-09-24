bda78e39b5d658eebdf6a9441b78cac6a3e4d49a
mmars2 <mar2cus@gmail.com>
2019-09-24 13:47:23 +0800

    second

---
+++ 02/program2.py
@@ -0,0 +1,3 @@
+celsiusTemp = float( input( "Please enter temperature in celsius: " ) )
+fahrenheitTemp = float( celsiusTemp * 1.8 ) + 32
+print(celsiusTemp,'degree celsius in fahrenheit is:',round(fahrenheitTemp,1))
